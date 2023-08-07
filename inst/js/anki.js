958013bf4eb50da8e7e412dbd33ad1fe914e4f8a
Kenji Sato <mail@kenjisato.jp>
2023-08-07 18:26:33 +0900

    anki script.

---
+++ inst/js/anki.js
@@ -0,0 +1,9 @@
+$('.anki-marker .anki-text').click(function(){
+  if ($(this).hasClass('open')) {
+    $(this).css('opacity', '0');
+  } else {
+    $(this).css('opacity', '1');
+  }
+    $(this).toggleClass('open');
+  });
+});
